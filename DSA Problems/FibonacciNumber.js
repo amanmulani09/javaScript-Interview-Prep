d59eb71d2810c26f8a1622381d3a095c45d4c572
amanmulani09 <mulaniaman0504@gmail.com>
2023-07-24 11:17:23 +0530

    Fibonacci Series

--- DSA Problems/FibonacciNumber.js
+++ DSA Problems/FibonacciNumber.js
@@ -19,8 +19,9 @@ var fib = function(n){
     const arr = [0,1];
     for(let i = 2; i<=n; i++){
         arr.push(arr[i-1] + arr[i-2]);
+        console.log(arr)
     }
     return arr[n]
 }
 
-fib(3)
+fib(5)
