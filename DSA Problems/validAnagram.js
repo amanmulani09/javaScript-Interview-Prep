9ede5777e2e44292cbdfdfb528585b562081f729
amanmulani09 <mulaniaman0504@gmail.com>
2023-07-25 10:20:39 +0530

    valid anagarm approach 2

--- DSA Problems/validAnagram.js
+++ DSA Problems/validAnagram.js
@@ -13,3 +13,20 @@ const findAnagram = function(s,l){
 
 
 findAnagram('rat','car')
+
+const findAnagram2 = function(s,t){
+    if(s.length !== t.length ) return false;
+    let obj1 = {};
+    let obj2 = {};
+    for(let i =0; i<s.length; i++){
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1; // used to check if any property exist in the Object before
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+        
+    }
+    for(let key in obj1){
+        if(obj1[key] !== obj2[key]) return false;
+    }
+    return true;
+}
+
+findAnagram2('rat','tar')
